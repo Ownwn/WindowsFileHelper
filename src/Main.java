afff2500c676c154b28adb41b763fa11484075d6
Owen <64725743+Ownwn@users.noreply.github.com>
2025-05-04 21:17:16 +1200

    Unzip

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,11 @@ import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
+    public static final Set<String> zipEndings = Set.of("zip", "7z", "tar");
+    public static final String sevenZipLocation = "C:\\Program Files\\7-Zip\\7z.exe";
     public static void main(String[] args) {
         if (!System.getProperty("os.name").toLowerCase().startsWith("windows")) {
             throw new RuntimeException("Only windows supported!");
@@ -20,15 +23,38 @@ public class Main {
         List<File> files = Arrays.asList(fileArray);
         File latestFile = files.stream().max(Comparator.comparingLong(File::lastModified)).get();
 
-        try {
-            showInExplorer(latestFile);
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException("Error opening file!", e);
-        }
+        extractAndShow(latestFile);
     }
 
-    public static void showInExplorer(File file) throws IOException, InterruptedException {
+    public static void showInExplorer(File file) {
         String[] openInExplorer = {"explorer", "/select,", file.getAbsolutePath()};
-        new ProcessBuilder(openInExplorer).start().waitFor();
+        runCommand(openInExplorer);
+    }
+
+    public static void extractAndShow(File file) {
+        if (zipEndings.stream().noneMatch(ending -> file.getName().endsWith(ending))) {
+            throw new RuntimeException("Invalid folder to be unzipped!");
+        }
+
+        File newFolder = file.getParentFile().toPath().resolve("extract").toFile();
+        if (newFolder.exists()) {
+            throw new RuntimeException("Extraction folder already exists!");
+        }
+
+        String[] unzip = {sevenZipLocation, "e", file.getAbsolutePath(), "-o" + newFolder.getAbsolutePath()};
+        runCommand(unzip);
+
+        showInExplorer(newFolder);
+    }
+
+    public static void runCommand(String[] command) {
+        try {
+            int res = new ProcessBuilder(command).start().waitFor();
+            if (res != 0) {
+                throw new RuntimeException("Command gave non-zero exit value " + command);
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Exception running command" + command);
+        }
     }
 }
